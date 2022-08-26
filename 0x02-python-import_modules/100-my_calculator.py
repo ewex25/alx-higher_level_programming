ff7748c6e294e2b21ca0300ea10416f3c3e0e181
ewex25 <ewex258@gmail.com>
2022-08-26 07:59:27 -0700

    task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-from calculator_1.py import add, sub, mul, div
-import sys
+
 
 if __name__ == "__main__":
-    if len(sys.argv)!= 4:
-        print("usage: ./100-my_calculator.py <a> <operator> <b>")
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     ops = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(ops.keys()):
@@ -12,12 +14,4 @@ if __name__ == "__main__":
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-
-    if sys.argv[2] == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif sys.argv[2] == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif sys.argv[2] == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif sys.argv[2] == '/':
-        print("{} / {} = {}".format(a, b, div(a, b))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))

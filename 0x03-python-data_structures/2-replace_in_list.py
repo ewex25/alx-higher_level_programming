8a3212bc374058160616be374336f9b4fb7fd2c5
ewex25 <ewex258@gmail.com>
2022-08-29 12:28:51 -0700

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx,element):
-    length_of_list = len(my_list) - 1
-    if (idx < 0 or idx > length_of_list):
-        return (my_list)
-    else:
-        my_list[idx] = element
-        return (my_list)
+
+
+def replace_in_list(my_list, idx, element):
+    if idx >= 0 and idx < len(my_list):
+        for i in range(len(my_list)):
+            my_list[idx] = element
+    return my_list

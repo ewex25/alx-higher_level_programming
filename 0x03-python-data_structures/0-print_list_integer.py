1e651becc8a9a0b4f0296ec248faa10ea06e1357
ewex25 <ewex258@gmail.com>
2022-08-29 11:52:22 -0700

    task

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for integers in my_list:
-        print("{:d}".format(integers))
+
+
+def square_matrix_simple(matrix=[]):
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
